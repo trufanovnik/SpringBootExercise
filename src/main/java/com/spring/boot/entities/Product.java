5cf0cce782eaa233556bb7e75b6a6176b8f6d0ba
unknown <trufanovwork@gmail.com>
2025-05-03 21:45:52 +0300

    add hibernate annotations for product

--- src/main/java/com/spring/boot/entities/Product.java
+++ src/main/java/com/spring/boot/entities/Product.java
@@ -1,13 +1,22 @@
 package com.spring.boot.entities;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "cost")
     private int cost;
 
     public Product(long id, String title, int cost) {
