62a902766087bc51932b5acacabf871b59311693
unknown <trufanovwork@gmail.com>
2025-05-04 08:58:44 +0300

    create ProductService with getAllProducts method

---
+++ src/main/java/com/spring/boot/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.spring.boot.services;
+
+import com.spring.boot.entities.Product;
+import com.spring.boot.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+}
