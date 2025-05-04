1084463dc8aa652d02b9d557766c1c5cdae05b39
unknown <trufanovwork@gmail.com>
2025-05-04 10:11:50 +0300

    add new product as model attribute and create add post-method

--- src/main/java/com/spring/boot/controllers/ProductController.java
+++ src/main/java/com/spring/boot/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package com.spring.boot.controllers;
 
+import com.spring.boot.entities.Product;
 import com.spring.boot.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +18,15 @@ public class ProductController {
 
     @GetMapping
     public String showProductsList(Model model){
+        Product product = new Product();
+        model.addAttribute("product", product);
         model.addAttribute("products", productService.getAllProducts());
         return "products";
+    }
+
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute(value = "product") Product product){
+        productService.add(product);
+        return "redirect:/products";
     }
 }
