94bf2be7a52313e242a4b78efd63d59de5faf18a
unknown <trufanovwork@gmail.com>
2025-05-09 17:29:46 +0300

    add JpaSpecificationExecutor for ProductRepository

--- src/main/java/com/spring/boot/repositories/ProductRepository.java
+++ src/main/java/com/spring/boot/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.spring.boot.repositories;
 
 import com.spring.boot.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 }
