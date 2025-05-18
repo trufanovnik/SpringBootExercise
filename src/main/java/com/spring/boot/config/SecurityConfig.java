c01ec24d4b17b7f1c65520fcbdabb6f62cba1e1f
unknown <trufanovwork@gmail.com>
2025-05-18 21:17:19 +0300

    add security config

---
+++ src/main/java/com/spring/boot/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.spring.boot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/products/add").hasAnyRole("ADMIN")
+                        .requestMatchers("/products/delete/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/products/edit/**").hasAnyRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/products"))
+                .exceptionHandling(handling -> handling
+                        .accessDeniedHandler("access-denied"));
+        return http.build();
+    }
+}
