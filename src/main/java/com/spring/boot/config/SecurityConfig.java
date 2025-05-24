2f65ece67ed368ba4d9630b999724944c1ca5adb
unknown <trufanovwork@gmail.com>
2025-05-24 20:02:54 +0300

    add login page and security config for login and logout

--- src/main/java/com/spring/boot/config/SecurityConfig.java
+++ src/main/java/com/spring/boot/config/SecurityConfig.java
@@ -4,12 +4,12 @@ import com.spring.boot.config.security.CustomAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.sql.DataSource;
 
@@ -29,20 +29,31 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/products/add").hasAnyRole("ADMIN")
-                        .requestMatchers("/products/delete/**").hasAnyRole("ADMIN")
-                        .requestMatchers("/products/edit/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/login", "/login?error").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .requestMatchers("/products/add", "/products/delete/**", "/products/edit/**").hasRole("ADMIN")
                         .requestMatchers("/access-denied").permitAll()
                         .anyRequest().permitAll()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/products", true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/products"))
-//                .exceptionHandling(Customizer.withDefaults()
+                        .logoutSuccessUrl("/products")
+                        .permitAll()
+                )
                 .exceptionHandling(handling -> handling
-                        .accessDeniedHandler(new CustomAccessDeniedHandler())
+                        .accessDeniedHandler(accessDeniedHandler())
                 );
         return http.build();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
     }
 }
